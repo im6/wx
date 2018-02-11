c6e476bf14e68c58715c2c6322c3c65920a280cf
im6 <ee2win@gmail.com>
2018-02-11 00:54:29 -0500

    simple uploader

---
+++ app.py
@@ -0,0 +1,35 @@
+import sys
+from os import path
+from auth import getBucket
+from oss2 import CaseInsensitiveDict
+from jobs import colorpk_config, colorpk2_config
+
+gzip_header = CaseInsensitiveDict({
+    "Content-Encoding": "gzip",
+    "Cache-Control": "public, max-age=172800"
+})
+
+bkt = getBucket()
+
+app = sys.argv[1]
+job_config = None
+if app == 'colorpk':
+    print('start colorpk...')
+    job_config = colorpk_config
+elif app == 'colorpk2':
+    print('start colorpk2...')
+    job_config = colorpk2_config
+else:
+    print('no app found.')
+
+for oneFile in job_config['files']:
+    filePath = path.normpath(path.join(job_config['src'], oneFile))
+    print('sending %s...' % filePath)
+    result = bkt.put_object_from_file('%s/%s'%(colorpk2_config['dst'], oneFile), filePath, gzip_header)
+    if result.status == 200:
+        print('success on %s' % oneFile)
+    else:
+        print('fail on %s' % oneFile)
+
+
+print('%s job finished.' % app)
---
+++ jobs.py
@@ -0,0 +1,33 @@
+colorpk_config = {
+    "src": "/Users/ZJ/PycharmProjects/vp/dist/",
+    "dst": "2",
+    "files": [
+        'app.js',
+        'vendor.js'
+    ],
+}
+
+colorpk2_config = {
+    "src": "/Users/ZJ/PycharmProjects/vp2/static/",
+    "dst": "4",
+    "files": [
+        'bundle0.js',
+        'bundle1.js',
+        'bundle2.js',
+        'bundle3.js',
+        'bundle4.js',
+        'bundle5.js',
+        'bundle0.css',
+        'bundle1.css',
+        'bundle2.css',
+        'bundle3.css',
+        'bundle4.css',
+        'bundle5.css',
+    ],
+}
+
+jsfun_config = {
+    "src": "",
+    "dst": "",
+    "files": [],
+}
