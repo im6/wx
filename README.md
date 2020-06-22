8ab7e4e77647d29a9220286f0ddfd8b34c6b229e
im6 <ee2win@gmail.com>
2020-06-21 21:40:30 -0400

    update folder process

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 source env/bin/activate
 CREDPATH=local/credentials.cnf python colorpk.py colorpk prod # for colorpk v1
 CREDPATH=local/credentials.cnf python colorpk.py colorpk2 prod # for colorpk v2
-CREDPATH=local/credentials.cnf python jsfun.py svelte.svg # for jsfun-icon
+CREDPATH=local/credentials.cnf python jsfun.py /Users/guo/Desktop/oss # for jsfun-icon, oss is folder name in Desktop
 ```
--- jsfun.py
+++ jsfun.py
@@ -1,9 +1,11 @@
 import sys
-from os import path
+import subprocess
+from os import path, walk
 from auth import getBucket
 from oss2 import CaseInsensitiveDict
 from jobs import colorpk_config, colorpk2_config
 
+folder_dir = sys.argv[1]
 gzip_header = CaseInsensitiveDict({
     "Content-Encoding": "gzip",
     "Cache-Control": "public, max-age=31104000"
@@ -11,16 +13,23 @@ gzip_header = CaseInsensitiveDict({
 
 bkt = getBucket()
 
-iconName = sys.argv[1]
+def upload_to_oss(fileName):
+  filePath = path.normpath(path.join(folder_dir, fileName))
+  print('sending %s...' % filePath)
+  result = bkt.put_object_from_file('1/icons/%s'%(fileName), filePath, gzip_header)
+  if result.status == 200:
+      print('success on %s' % fileName)
+  else:
+      print('fail on %s' % fileName)
 
-print('start jsfun-icon...')
-filePath = path.normpath(path.join('/Users/guo/Desktop', iconName))
-print('sending %s...' % filePath)
-result = bkt.put_object_from_file('1/icons/%s'%(iconName), filePath, gzip_header)
-if result.status == 200:
-    print('success on %s' % iconName)
-else:
-    print('fail on %s' % iconName)
+print('start jsfun jobs...')
 
+for subdir, dirs, files in walk(folder_dir):
+  for i in files:
+    if i == ".DS_Store":
+      continue
+    subprocess.call(["gzip", "%s/%s" % (folder_dir, i)])
+    subprocess.call(["mv", "%s/%s.gz" % (folder_dir, i), "%s/%s" % (folder_dir, i)])
+    upload_to_oss(i)
 
 print('job finished.')
