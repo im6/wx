5c607329b0136fbc9732cc9c9e7dbbcd3f52dc9a
im6 <ee2win@gmail.com>
2025-02-16 20:57:03 -0500

    update

---
+++ wx/wxApp.swift
@@ -0,0 +1,17 @@
+//
+//  wxApp.swift
+//  wx
+//
+//  Created by Zijian Guo on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct wxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wxTests/wxTests.swift
@@ -0,0 +1,17 @@
+//
+//  wxTests.swift
+//  wxTests
+//
+//  Created by Zijian Guo on 2/16/25.
+//
+
+import Testing
+@testable import wx
+
+struct wxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
